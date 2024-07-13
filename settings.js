5b4a702e27a85930a063e39e780921af9cc04761
Blackbrood <96950354+Blackbrood@users.noreply.github.com>
2024-07-13 01:31:25 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,18 +4,18 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "916909137213"
-global.ownername = "David Cyril"
+global.ownernomer = "2348078112891"
+global.ownername = "Caesar"
 global.ytname = "David Cyril"
 global.socialm = "GitHub: DeeCeeXxx"
 global.location = "Africa, Nigeria, Lagos"
 
-global.ownernumber = '2347043759577,2349066528353,254782572110'  //creator number
-global.ownername = 'David Cyril' //owner name
+global.ownernumber = '2348078112891'  //creator number
+global.ownername = 'Cyril' //owner name
 global.botname = '🕷Venom-2🕷' //name of the bot
 
 //sticker details
-global.packname = 'Sticker By'
+global.packname = 'Caesar'
 global.author = 'David_Cyril\n\nContact: +2349066528353'
 
 //console view/theme
@@ -26,7 +26,7 @@ global.wm = "David Cyril Tech."
 global.link = 'https://whatsapp.com/channel/0029VaeRru3ADTOEKPCPom0L'
 
 //custom prefix
-global.prefa = ['','!','.','#','&']
+global.prefa = ['!']
 
 //false=disable and true=enable
 global.autoRecording = false //auto recording
